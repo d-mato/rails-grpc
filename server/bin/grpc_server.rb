a1d1f289eed62585bd41fb28c463dee02652068b
Daiki Matoba <telnetstat@gmail.com>
2019-06-29 14:00:09 +0900

    Create server

---
+++ server/bin/grpc_server.rb
@@ -0,0 +1,28 @@
+$LOAD_PATH.push('./lib')
+
+require 'helloworld_services_pb'
+
+class GreeterServer < Helloworld::Greeter::Service
+
+  def say_hello(hello_req, _unused_call)
+    Helloworld::HelloReply.new(message: "Hello #{hello_req.name}")
+  end
+
+  def say_hello_again(hello_req, _unused_call)
+    Helloworld::HelloReply.new(message: "Hello again, #{hello_req.name}")
+  end
+end
+
+# main starts an RpcServer that receives requests to GreeterServer at the sample
+# server port.
+def main
+  s = GRPC::RpcServer.new
+  s.add_http2_port('0.0.0.0:50051', :this_port_is_insecure)
+  s.handle(GreeterServer)
+  # Runs the server with SIGHUP, SIGINT and SIGQUIT signal handlers to
+  #   gracefully shutdown.
+  # User could also choose to run server via call to run_till_terminated
+  s.run_till_terminated_or_interrupted([1, 'int', 'SIGQUIT'])
+end
+
+main
